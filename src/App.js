5f82e0cf7e3b3acdaa6c83e7f824a989410fcbb6
thahir <abuthahircoorg07@gmail.com>
2024-04-15 22:25:42 +0530

    Age Calculator

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [birthDate, setBirthDate]=useState("");
+  const [years, setYears]=useState("");
+
+  function calculateDate(birthDate){
+    const currentDate= new Date();
+    const diffTime=currentDate-new Date(birthDate);
+    const totalDays=Math.floor(diffTime/(1000*60*60*24));
+    setYears(Math.floor(totalDays/365.25));
+  }
+  function update(){
+    calculateDate(birthDate);
+  }
+  return (
+    <div className="App">
+        <h1>Age Calculator</h1>
+        <p>Enter your date of birth</p>
+        <input
+        type='date' 
+        value={birthDate}
+        onChange={(e) =>{
+          setBirthDate(e.target.value);
+        }}/><br/>
+        <button type='secondary' className='btn' onClick={update}>Calculate Age</button>
+        {years?<h3>You are {years} years old</h3>:""}
+    </div>
+  );
+}
+
+export default App;
